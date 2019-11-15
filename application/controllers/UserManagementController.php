788a396f56a6dd3ffbc8386938dac5094a929ef0
RaveenDharmasiri <raveen.dharmasiri@gmail.com>
2019-11-16 02:30:08 +0530

    Created the register function

--- application/controllers/UserManagementController.php
+++ application/controllers/UserManagementController.php
@@ -3,7 +3,26 @@
 class UserManagementController extends CI_Controller
 {
     public function index()
-    { 
+    {
         $this->load->view('properties/register');
+    }
+
+    public function insertUser()
+    {
+        $firstName  = $this->input->post('firstName');
+        $lastName = $this->input->post('lastName');
+        $email = $this->input->post('email');
+        $password = $this->input->post('password');
+
+        $this->load->model('Register');
+        $emailInstanceCount = $this->Register->checkIfEmailAlreadyExists($email);
+        if ($emailInstanceCount > 0) {
+            $returnData = array(
+                'errorMessage' => 'Email Already Exists',
+            );
+            $this->load->view('properties/register', $returnData);
+        } else {
+            $this->Register->insertUser($firstName, $lastName, $email, $password);
+        }
     }
 }

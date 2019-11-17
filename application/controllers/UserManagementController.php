d2c519fadee21c79c314e3c38c0e38ee88973f60
RaveenDharmasiri <raveen.dharmasiri@gmail.com>
2019-11-17 22:05:52 +0530

    image upload done, multiselect dropdown menu done

--- application/controllers/UserManagementController.php
+++ application/controllers/UserManagementController.php
@@ -27,7 +27,8 @@ class UserManagementController extends CI_Controller
         }
     }
 
-    public function findUser() {
+    public function findUser()
+    {
         $email = $this->input->get('email');
         $password = $this->input->get('password');
 
@@ -38,10 +39,67 @@ class UserManagementController extends CI_Controller
             $this->load->view('properties/home');
         } else {
             $viewReturnData = array(
-                'errorMessage'=>'Email or Password you entered are not correct',
+                'errorMessage' => 'Email or Password you entered are not correct',
             );
 
             $this->load->view('properties/login', $viewReturnData);
         }
+    }
+
+    public function editProfileInfoUpdate()
+    {
+        $genres = $this->input->post('genres');
+        $image = $this->input->post('profileImage');
+
+        $email = $this->input->post('userEmail');
+
+        $this->uploadImageToFolder($email);
+    }
+
+    private function uploadImageToFolder($email)
+    {
+        $config['upload_path'] = './uploads/';
+        $config['allowed_types'] = 'gif|jpg|png';
+
+        $this->load->library('upload', $config);
+        $this->upload->initialize($config);
+        if (!$this->upload->do_upload('profileImage')) {
+            $error = array('error' => $this->upload->display_errors());
+            // var_dump($error['error']);
+            $returnArray = array(
+                'error'=>$error['error'],
+            );
+            $this->load->view('properties/editProfile', $returnArray);
+        } else {
+            $imageData = array('upload_data' => $this->upload->data());
+
+            $relativeImagePath = $this->uploadImageToDB($imageData);
+
+            $returnArray = array(
+                'relativeImagePath'=>$relativeImagePath,
+            );
+
+            $this->load->view('properties/editProfile', $returnArray);
+
+            // var_dump($imageData['upload_data']['full_path']);
+
+            // $this->load->view('upload_success', $data);
+        }
+    }
+
+    private function uploadImageToDB($imageData) {
+        $imageFullPath = $imageData['upload_data']['full_path'];
+        $relativeImagePath = $this->getProfileImageRelativePath($imageFullPath);
+        return $relativeImagePath;
+    }
+
+    private function getProfileImageRelativePath($imageFullPath) {
+        $splitImageFullPath = explode('/', $imageFullPath);
+        // var_dump($splitImageFullPath);
+
+        $relativeImagePath = $splitImageFullPath[6].'/'.$splitImageFullPath[7];
+        // var_dump($relativeImagePath);
+
+        return $relativeImagePath;
     }
 }
--- application/views/properties/editProfile.php
+++ application/views/properties/editProfile.php
@@ -5,10 +5,17 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.11/css/bootstrap-select.min.css" integrity="sha256-xX+DsGeZhhgAtFGlA5iy4tpVy7wgoXKcFu13+B1qh7k=" crossorigin="anonymous" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.11/js/bootstrap-select.min.js" integrity="sha256-49+cFFN14Ib7A61zjYlhc4UnmbAPQ0uImp4Sj4JO8TU=" crossorigin="anonymous"></script>
+
+
+
 <link rel="stylesheet" href="<?php echo base_url('assets/css/editProfile.css'); ?>" />
 <script type="text/javascript" src="<?php echo base_url('assets/js/scripts.js'); ?>"></script>
+<!-- <script type="text/javascript" src="<?php echo base_url('assets/js/editProfileScripts.js'); ?>"></script> -->
 
-<h1 class="h3 mb-3 font-weight-sitename">Your Profile</h1>
+<h1 class="h3 mb-3 font-weight-sitename">Edit Profile</h1>
 <div class="container">
     <div class="row flex-lg-nowrap">
         <div class="col-12 col-lg-auto mb-3" style="width: 200px;">
@@ -19,15 +26,33 @@
             <div class="row">
                 <div class="col mb-3">
                     <div class="card">
+                        <div class="image-error-message">
+                            <?php
+                            if (isset($error)) {
+                                echo "<p>" . $error . "</p>";
+                            }
+                            ?>
+                        </div>
+
                         <div class="card-body">
-                            <div class="e-profile">
+                            <form class="e-profile" action='<?php echo base_url('/index.php/UserManagementController/editProfileInfoUpdate'); ?>' enctype='multipart/form-data' method='POST'>
                                 <div class="row">
                                     <div class="col-12 col-sm-auto mb-3">
                                         <div class="mx-auto" style="width: 140px;">
-                                            <img src="<?php echo base_url('assets/images/profileimage-placeholder.png'); ?>" id="profileDisplay">
+                                            <img src="<?php 
+                                            if (isset($relativeImagePath)) {
+                                                echo base_url($relativeImagePath);
+                                            } else {
+                                                echo base_url('assets/images/profileimage-placeholder.png');
+                                            }
+                                            ?>"
+                                            id="profileDisplay">
+                                            
                                             <input type="file" id="profileImage" name="profileImage" onchange="displayImage(this)" />
+
                                         </div>
                                     </div>
+
                                     <div class="col d-flex flex-column flex-sm-row justify-content-between mb-3">
                                         <div class="text-center text-sm-left mb-2 mb-sm-0">
                                             <h4 class="pt-sm-2 pb-1 mb-0 text-nowrap">John Smith</h4>
@@ -40,13 +65,13 @@
                                         </div>
                                     </div>
                                 </div>
-                                <ul class="nav nav-tabs">
-                                    <li class="nav-item"><a href="" class="active nav-link">Settings</a></li>
+                                <ul class="nav nav-tabs" hidden>
+                                    <li class="nav-item"><a href="" class="active nav-link"></a></li>
                                 </ul>
                                 <div class="tab-content pt-3">
                                     <div class="tab-pane active">
-                                        <form class="form" novalidate="">
-                                            <div class="row">
+                                        <div class="form" novalidate="">
+                                            <!-- <div class="row">
                                                 <div class="col">
                                                     <div class="row">
                                                         <div class="col">
@@ -79,52 +104,49 @@
                                                         </div>
                                                     </div>
                                                 </div>
-                                            </div>
+                                            </div> -->
+                                            <input class="form-control" type="email" name="userEmail" hidden/>
+
                                             <div class="row">
                                                 <div class="col-12 col-sm-6 mb-3">
-                                                    <div class="mb-2"><b>Change Password</b></div>
-                                                    <div class="row">
-                                                        <div class="col">
-                                                            <div class="form-group">
-                                                                <label>Current Password</label>
-                                                                <input class="form-control" type="password" placeholder="••••••">
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                    <div class="row">
-                                                        <div class="col">
-                                                            <div class="form-group">
-                                                                <label>New Password</label>
-                                                                <input class="form-control" type="password" placeholder="••••••">
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                    <div class="row">
-                                                        <div class="col">
-                                                            <div class="form-group">
-                                                                <label>Confirm <span class="d-none d-xl-inline">Password</span></label>
-                                                                <input class="form-control" type="password" placeholder="••••••"></div>
-                                                        </div>
-                                                    </div>
+                                                    <label class="select-menu-title">Select your favourite Genres</label>
+                                                    <!-- <label class="text-white mb-3 lead">Select your favourite genres</label> -->
+                                                    <!-- Multiselect dropdown -->
+                                                    <select multiple data-style="bg-white rounded-pill px-4 py-3 shadow-sm " class="selectpicker w-100" name="genres[]">
+                                                        <option name="CountryMusic" value="Country Music">Country Music</option>
+                                                        <option name="ClassicalMusic" value="Classical Music">Classical Music</option>
+                                                        <option name="PopMusic" value="Pop Music">Pop Music</option>
+                                                        <option name="Disco" value="Disco">Disco</option>
+                                                        <option name="Rock" value="Rock">Rock</option>
+                                                    </select><!-- End -->
                                                 </div>
-
+                                                <!-- <div class="col-12 col-sm-5 offset-sm-1 mb-3">
+                                                    <div class="row">
+                                                        <div class="col">
+                                                            <div class="form-group">
+                                                                <label class="genre-list-title">Your favourite Genres</label>
+                                                                
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div> -->
                                             </div>
                                             <div class="row">
                                                 <div class="col d-flex justify-content-end">
                                                     <button class="btn btn-primary" type="submit">Save Changes</button>
                                                 </div>
                                             </div>
-                                        </form>
+                                        </div>
 
                                     </div>
                                 </div>
-                            </div>
+                            </form>
                         </div>
                     </div>
                 </div>
 
                 <div class="col-12 col-md-3 mb-3">
-                    <div class="card mb-3">
+                    <!-- <div class="card mb-3">
                         <div class="card-body">
                             <div class="px-xl-3">
                                 <button class="btn btn-block btn-secondary">
@@ -133,7 +155,7 @@
                                 </button>
                             </div>
                         </div>
-                    </div>
+                    </div> -->
                 </div>
             </div>
 

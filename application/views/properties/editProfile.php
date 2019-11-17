a41a77b99e3db442ba310664f2cdce635aa27f1a
RaveenDharmasiri <raveen.dharmasiri@gmail.com>
2019-11-17 07:11:43 +0530

    Picture preview is done

--- application/views/properties/editProfile.php
+++ application/views/properties/editProfile.php
@@ -4,20 +4,15 @@
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
 <link rel="stylesheet" href="<?php echo base_url('assets/css/editProfile.css'); ?>" />
+<script type="text/javascript" src="<?php echo base_url('assets/js/scripts.js'); ?>"></script>
+
 <h1 class="h3 mb-3 font-weight-sitename">Your Profile</h1>
 <div class="container">
     <div class="row flex-lg-nowrap">
         <div class="col-12 col-lg-auto mb-3" style="width: 200px;">
-            <!-- <div class="card p-3">
-                <div class="e-navlist e-navlist--active-bg">
-                    <ul class="nav">
-                        <li class="nav-item"><a class="nav-link px-2 active" href="./overview.html"><i class="fa fa-fw fa-bar-chart mr-1"></i><span>Overview</span></a></li>
-                        <li class="nav-item"><a class="nav-link px-2" href="./users.html"><i class="fa fa-fw fa-th mr-1"></i><span>CRUD</span></a></li>
-                        <li class="nav-item"><a class="nav-link px-2" href="./settings.html"><i class="fa fa-fw fa-cog mr-1"></i><span>Settings</span></a></li>
-                    </ul>
-                </div>
-            </div> -->
+
         </div>
 
         <div class="col">
@@ -29,18 +24,15 @@
                                 <div class="row">
                                     <div class="col-12 col-sm-auto mb-3">
                                         <div class="mx-auto" style="width: 140px;">
-                                            <div class="d-flex justify-content-center align-items-center rounded" style="height: 140px; background-color: rgb(233, 236, 239);">
-                                                <span style="color: rgb(166, 168, 170); font: bold 8pt Arial;">Profile Image Empty</span>
-                                            </div>
-
-                                            <input type="file" id="profileImageUpload" hidden/> 
+                                            <img src="<?php echo base_url('assets/images/profileimage-placeholder.png'); ?>" id="profileDisplay">
+                                            <input type="file" id="profileImage" name="profileImage" onchange="displayImage(this)" />
                                         </div>
                                     </div>
                                     <div class="col d-flex flex-column flex-sm-row justify-content-between mb-3">
                                         <div class="text-center text-sm-left mb-2 mb-sm-0">
                                             <h4 class="pt-sm-2 pb-1 mb-0 text-nowrap">John Smith</h4>
                                             <div class="mt-2">
-                                                <button for="profileImageUpload" class="btn btn-primary" type="button" >
+                                                <button for="profileImageUpload" class="btn btn-primary" type="button" onclick="triggerClick()">
                                                     <i class="fa fa-fw fa-camera"></i>
                                                     <span>Change Photo</span>
                                                 </button>
@@ -115,7 +107,7 @@
                                                         </div>
                                                     </div>
                                                 </div>
-                                                
+
                                             </div>
                                             <div class="row">
                                                 <div class="col d-flex justify-content-end">
@@ -142,13 +134,6 @@
                             </div>
                         </div>
                     </div>
-                    <!-- <div class="card">
-                        <div class="card-body">
-                            <h6 class="card-title font-weight-bold">Support</h6>
-                            <p class="card-text">Get fast, free help from our friendly assistants.</p>
-                            <button type="button" class="btn btn-primary">Contact Us</button>
-                        </div>
-                    </div> -->
                 </div>
             </div>
 
